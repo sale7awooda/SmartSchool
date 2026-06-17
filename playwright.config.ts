9ce08c9d850525216b7a46c0ac4b624d2cdb4a11
Sale7awooda <sale7awooda@gmail.com>
2026-06-18 02:45:44 +0300

    fix: move playwright timeout to top-level config option

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests/e2e',
+  timeout: 60000,
   fullyParallel: false,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
@@ -11,7 +12,6 @@ export default defineConfig({
     baseURL: 'http://localhost:3000',
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
-    timeout: 60000,
   },
   projects: [
     {
